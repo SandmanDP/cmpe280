e5450fc87e2ee38cdcb04762115ee1564685944c
Isaac <isaac.wahhab@gmail.com>
2019-04-07 00:48:39 -0700

    Foundation for map features

--- application.py
+++ application.py
@@ -8,3 +8,12 @@ application = Flask(__name__)
 @application.route('/')
 def entry():
     return application.send_static_file('home.html')
+
+
+@application.route('/map')
+def map():
+    return application.send_static_file('map.html')
+
+
+if __name__ == '__main__':
+    application.run(debug=True)

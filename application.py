65d8e9dc6b42d4ee6257ce5d48de3edc860850ac
Isaac <isaac.wahhab@gmail.com>
2019-04-06 22:06:32 -0700

    Feeling lucky

--- application.py
+++ application.py
@@ -2,14 +2,10 @@
 from flask import Flask
 
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
 @app.route('/')
 def entry():
     return "Hello World!"
     # return app.send_static_file('home.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)

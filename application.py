79ec8cefd9f767b0c39db31f2782f968045e54c2
Isaac <isaac.wahhab@gmail.com>
2019-04-06 21:31:42 -0700

    Reduced FLASK initial test

--- application.py
+++ application.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def entry():
-    return app.send_static_file('home.html')
+    return "Hello World!"
+    # return app.send_static_file('home.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
